9f5a5e36dc6e12819e589e6d0913234b098983cc
steve <stevetak@outlook.fr>
2025-03-16 19:38:44 +0100

    edit log-recup-user-60%

--- Server/app/middleware/auth_middleware.ts
+++ Server/app/middleware/auth_middleware.ts
@@ -2,24 +2,36 @@ import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
 import type { Authenticators } from '@adonisjs/auth/types'
 
+
 /**
- * Auth middleware is used authenticate HTTP requests and deny
+ * Auth middleware is used to authenticate HTTP requests and deny
  * access to unauthenticated users.
  */
 export default class AuthMiddleware {
-  /**
-   * The URL to redirect to, when authentication fails
-   */
-  redirectTo = '/login'
+ 
 
-  async handle(
+  redirectTo = '/signup'
+
+  public async handle(
     ctx: HttpContext,
     next: NextFn,
     options: {
       guards?: (keyof Authenticators)[]
     } = {}
   ) {
-    await ctx.auth.authenticateUsing(options.guards, { loginRoute: this.redirectTo })
-    return next()
+    try {
+      //Vérifie si l'utilisateur est authentifié
+      await ctx.auth.authenticateUsing(options.guards, { loginRoute: this.redirectTo })
+
+      //Si l'utilisateur est authentifié, passe à la suite
+      await next()
+    } catch (error) {
+
+      //Si échec → réponse 401 Unauthorized
+      ctx.response.status(401).send({
+        error: 'Unauthorized',
+        message: error.message,
+      })
+    }
   }
 }
--- Server/config/auth.ts
+++ Server/config/auth.ts
@@ -3,15 +3,16 @@ import { tokensGuard, tokensUserProvider } from '@adonisjs/auth/access_tokens'
 import type { InferAuthenticators, InferAuthEvents, Authenticators } from '@adonisjs/auth/types'
 
 const authConfig = defineConfig({
-  default: 'api',
+  default: 'api',  //Définir le type d'authentification par défaut
   guards: {
     api: tokensGuard({
       provider: tokensUserProvider({
         tokens: 'accessTokens',
-        model: () => import('#models/user')
+        model: () => import('#models/user') // Modèle utilisateur lié au token
       }),
     }),
   },
+ 
 })
 
 export default authConfig
@@ -23,6 +24,7 @@ export default authConfig
 declare module '@adonisjs/auth/types' {
   export interface Authenticators extends InferAuthenticators<typeof authConfig> {}
 }
+
 declare module '@adonisjs/core/types' {
   interface EventsList extends InferAuthEvents<Authenticators> {}
 }
